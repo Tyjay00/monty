72eecf758bb983221ccb699e91b88c48f3283c94
Zwivhuya Muthathi <88210910+Vhazee@users.noreply.github.com>
2023-08-25 19:49:37 +0200

    Update addnode.c

--- addnode.c
+++ addnode.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * addnode - add node to the head stack
+ * addnode - function adds a node to the head stack
  * @head: head of the stack
  * @n: new_value
  * this function adds a new node containing the value n
